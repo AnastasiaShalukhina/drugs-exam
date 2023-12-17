aa80869c8aaec1ad451c9abb41ba030b962ba7c4
AnastasiaShalukhina <exachka4@gmail.com>
2023-12-17 22:27:24 +0300

    feat: add slide2

---
+++ modules/slides/Slide2.js
@@ -0,0 +1,11 @@
+import { createSlide } from '../base/Slide.js';
+
+export function createSlide2() {
+
+  const slide = createSlide();
+  slide.classList.add('slide', 'slide-raw')
+
+
+
+  return slide;
+}
